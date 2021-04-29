d4060b6578c259537ba0a23cfd8f22296c1273ba
chardwillian <chardwillian9@gmail.com>
2021-04-28 23:51:15 -0300

    Melhorando backend com classToClass

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -12,17 +13,7 @@ export default class ProfileController {
 
         const user = await showProfile.execute({ user_id });
 
-        // delete user.password;
-
-        const userWithoutPassword = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            created_at: user.created_at,
-            updated_at: user.updated_at,
-        };
-
-        return response.json(userWithoutPassword);
+        return response.json(classToClass(user));
     }
 
     public async update(
@@ -42,14 +33,6 @@ export default class ProfileController {
             password,
         });
 
-        const userWithoutPassword = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            created_at: user.created_at,
-            updated_at: user.updated_at,
-        };
-
-        return response.json(userWithoutPassword);
+        return response.json(classToClass(user));
     }
 }
--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 export default class UserAvatarController {
@@ -14,11 +15,6 @@ export default class UserAvatarController {
             avatarFilename: request.file.filename,
         });
 
-        //  const { password: saaa, ...userWithoutPassword } = user;
-
-        //  return response.json({ user: userWithoutPassword });
-        // delete user.password;
-
-        return response.json(user);
+        return response.json(classToClass(user));
     }
 }
